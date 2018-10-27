763eccc99705c35da4446e936f2e3c61dfa9e621
Renato Ribeiro <r3nato.rp+github@gmail.com>
2018-10-26 22:41:31 -0300

    Creating parameters to be sent by dropzone

--- src/main/java/com/example/uploadit/resource/FileResource.java
+++ src/main/java/com/example/uploadit/resource/FileResource.java
@@ -4,11 +4,8 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.multipart.MultipartFile;
 
 import com.example.uploadit.vo.FileRequestBody;
 
--- src/main/java/com/example/uploadit/vo/FileRequestBody.java
+++ src/main/java/com/example/uploadit/vo/FileRequestBody.java
@@ -7,9 +7,14 @@ import org.springframework.web.multipart.MultipartFile;
 public class FileRequestBody implements Serializable {
 
 	private static final long serialVersionUID = 1L;
-	
+
 	private MultipartFile file;
 	private String dzuuid;
+	private Integer dzchunkindex;
+	private Long dztotalfilesize;
+	private Long dzchunksize;
+	private Integer dztotalchunkcount;
+	private Integer dzchunkbyteoffset;
 
 	public String getDzuuid() {
 		return dzuuid;
@@ -26,7 +31,45 @@ public class FileRequestBody implements Serializable {
 	public void setFile(MultipartFile file) {
 		this.file = file;
 	}
-	
-	
+
+	public Integer getDzchunkindex() {
+		return dzchunkindex;
+	}
+
+	public void setDzchunkindex(Integer dzchunkindex) {
+		this.dzchunkindex = dzchunkindex;
+	}
+
+	public Long getDztotalfilesize() {
+		return dztotalfilesize;
+	}
+
+	public void setDztotalfilesize(Long dztotalfilesize) {
+		this.dztotalfilesize = dztotalfilesize;
+	}
+
+	public Long getDzchunksize() {
+		return dzchunksize;
+	}
+
+	public void setDzchunksize(Long dzchunksize) {
+		this.dzchunksize = dzchunksize;
+	}
+
+	public Integer getDztotalchunkcount() {
+		return dztotalchunkcount;
+	}
+
+	public void setDztotalchunkcount(Integer dztotalchunkcount) {
+		this.dztotalchunkcount = dztotalchunkcount;
+	}
+
+	public Integer getDzchunkbyteoffset() {
+		return dzchunkbyteoffset;
+	}
+
+	public void setDzchunkbyteoffset(Integer dzchunkbyteoffset) {
+		this.dzchunkbyteoffset = dzchunkbyteoffset;
+	}
 
 }
