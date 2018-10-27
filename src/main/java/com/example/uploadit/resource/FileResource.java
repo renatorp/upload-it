6e4af7ff8c1ec2b093a11749f2d5ae50fd63d58c
Renato Ribeiro <r3nato.rp+github@gmail.com>
2018-10-26 23:02:16 -0300

    Creating backend file service

--- src/main/java/com/example/uploadit/resource/FileResource.java
+++ src/main/java/com/example/uploadit/resource/FileResource.java
@@ -1,5 +1,6 @@
 package com.example.uploadit.resource;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -8,17 +9,22 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.uploadit.service.IFileService;
 import com.example.uploadit.vo.FileRequestBody;
 
 @RestController
 @RequestMapping("files")
 public class FileResource {
 
-	
+	@Autowired
+	private IFileService fileService;
+
 	@CrossOrigin
 	@PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-    public ResponseEntity<?> handleFileUpload(@RequestParam("user") String userId, FileRequestBody requestBody) {
+    public ResponseEntity<?> handleFileChunkUpload(@RequestParam("user") String userId, FileRequestBody requestBody) {
 
+		fileService.uploadFileChunk(requestBody, userId);
+		
 		return ResponseEntity.ok().build();
     }
 	
