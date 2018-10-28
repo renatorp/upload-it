307a6109e44cf2e5f2f9138ff4c204405f44cfd7
Renato Ribeiro <r3nato.rp+github@gmail.com>
2018-10-28 18:54:40 -0300

    Improvements

--- src/main/java/com/example/uploadit/config/SwaggerConfig.java
+++ src/main/java/com/example/uploadit/config/SwaggerConfig.java
@@ -2,8 +2,6 @@ package com.example.uploadit.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
@@ -17,7 +15,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
 @EnableSwagger2
-public class SwaggerConfig extends WebMvcConfigurationSupport {
+public class SwaggerConfig {
 
 	@Bean
 	public Docket api() {
@@ -40,13 +38,4 @@ public class SwaggerConfig extends WebMvcConfigurationSupport {
                 .build();
     }
     
-	@Override
-	public void addResourceHandlers(ResourceHandlerRegistry registry) {
-	    registry.addResourceHandler("swagger-ui.html")
-	      .addResourceLocations("classpath:/META-INF/resources/");
-	 
-	    registry.addResourceHandler("/webjars/**")
-	      .addResourceLocations("classpath:/META-INF/resources/webjars/");
-	}
-	
 }
